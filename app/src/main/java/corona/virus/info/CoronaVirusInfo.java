2fe250d0c19a2708a0de713fd706a99e56908756
choiman1559 <cuj1559@gmail.com>
2020-05-10 21:56:59 +0900

    fixed bugs

--- app/src/main/java/corona/virus/info/CoronaVirusInfo.java
+++ app/src/main/java/corona/virus/info/CoronaVirusInfo.java
@@ -1,32 +1,75 @@
 package corona.virus.info;
 
+import android.os.Handler;
+import android.util.Log;
+
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresPermission;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static android.Manifest.permission.INTERNET;
 
 public class CoronaVirusInfo {
 
     @Retention(RetentionPolicy.SOURCE)
-    @IntDef({TOTAL, UNDER_TREATMENT, RECOVERED,DEAD})
-    public @interface TYPE {}
+    @IntDef({TOTAL, ACTIVE, RECOVERED, DEAD})
+    public @interface TYPE { }
 
-    public static final int TOTAL = 0;
-    public static final int UNDER_TREATMENT = 1;
-    public static final int RECOVERED = 2;
-    public static final int DEAD = 3;
+    public static final int ACTIVE = 4;
+    public static final int RECOVERED = 5;
+    public static final int DEAD = 6;
+    public static final int TOTAL = 7;
 
-    public int getInt(@Nullable @CountryCode.Country String Country, @TYPE int TYPE) throws IOException {
-        URLConnection connection = new URL("https://api.covid19api.com/summary").openConnection();
-        try(Scanner scanner = new Scanner(connection.getInputStream());){
-            String response = scanner.useDelimiter("\\A").next();
-            System.out.println(response);
+    volatile String temp = null;
+
+    @RequiresPermission(INTERNET)
+    public int getInt(@Nullable @CountryCode.Country String Country, @TYPE int TYPE) {
+        String url = !Country.equals("") ? "https://api.covid19api.com/country/" + Country : "https://api.covid19api.com/summary";
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.newCall(new Request.Builder().url(url).build()).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException ignore) { throw new RuntimeException("request Failed while get HTTP body!");}
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull final Response response) {
+                try {
+                    temp = response.body().string();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        while(temp == null) { }
+        String[] lines = temp.split(Objects.requireNonNull(System.getProperty("line.separator")));
+        return Integer.parseInt(Between(lines[lines.length - TYPE],": ",","));
+    }
+
+    private String Between(String str, String open, String close) {
+        if (str == null || open == null || close == null) {
+            return null;
         }
-        return 0;
+        int start = str.indexOf(open);
+        if (start != -1) {
+            int end = str.indexOf(close, start + open.length());
+            if (end != -1) {
+                return str.substring(start + open.length(), end);
+            }
+        }
+        return null;
     }
 }
