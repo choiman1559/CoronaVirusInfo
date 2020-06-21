e0cb64d0776c757a38752641ad22655fcbbd1942
choiman1559 <cuj1559@gmail.com>
2020-06-21 23:01:12 +0900

    fixed bugs

--- app/src/main/java/corona/virus/info/CoronaVirusInfo.java
+++ app/src/main/java/corona/virus/info/CoronaVirusInfo.java
@@ -1,12 +1,11 @@
 package corona.virus.info;
 
-import android.util.Log;
-
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresPermission;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -82,9 +81,80 @@ public class CoronaVirusInfo {
         stopwatch.add(Calendar.SECOND,limitMs / 1000);
 
         while (temp == null) { if(stopwatch.getTimeInMillis() - System.currentTimeMillis() <= 0) return -2; }
+        String json = temp;
+        if(json == null) return -1;
+        temp = null;
+
+        try {
+            JSONArray array = new JSONArray(json);
+            if (!Country.equals("") && date == null) {
+                JSONObject data = new JSONObject(array.get(array.length()).toString());
+                return data.getInt(getType(TYPE));
+            }
+
+            for(int i = 0;i < array.length();i++) {
+                JSONObject data = new JSONObject(array.get(i).toString());
+                if(data.getString("Date").contains(new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date.getTime()))) {
+                    return data.getInt(getType(TYPE));
+                }
+            }
+
+            String[] lines = json.split(Objects.requireNonNull(System.getProperty("line.separator")));
+
+            if(Country.equals("")) switch (TYPE) {
+                case ACTIVE:
+                    return Integer.parseInt(Between(lines[3])) -
+                            (Integer.parseInt(Between(lines[7])) +
+                                    Integer.parseInt(Between(lines[5])));
+
+                case RECOVERED:
+                    return Integer.parseInt(Between(lines[7]));
+
+                case DEAD:
+                    return Integer.parseInt(Between(lines[5]));
+
+                case TOTAL:
+                    return Integer.parseInt(Between(lines[3]));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+       return -1;
+    }
+
+    @Deprecated
+    @RequiresPermission(INTERNET)
+    public int getInt_old(@CountryCode.Country_withoutGlobal String Country, @TYPE int TYPE, @Nullable Calendar date, int limitMs) {
+        if(Country.equals("") && date != null)
+            throw new IllegalArgumentException("if you're using \"Country.Global\" argument, value of \"Calender date\" must be always \"null\"!");
+        String url = !Country.equals("") ? "https://api.covid19api.com/country/" + Country : "https://api.covid19api.com/summary";
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.newCall(new Request.Builder().url(url).build()).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException ignore) {
+                throw new RuntimeException(ignore.getMessage(),ignore.getCause());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull final Response response) {
+                try {
+                    temp = response.body().string();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Calendar stopwatch = Calendar.getInstance();
+        stopwatch.setTime(new Date(System.currentTimeMillis()));
+        stopwatch.add(Calendar.SECOND,limitMs / 1000);
+
+        while (temp == null) { if(stopwatch.getTimeInMillis() - System.currentTimeMillis() <= 0) return -2; }
         String[] lines = null;
-        try { lines = new JSONObject(temp).toString().split(Objects.requireNonNull(System.getProperty("line.separator")));
-        Log.d("json",new JSONObject(temp).toString()); }
+        try { lines = new JSONObject(temp).toString().split(Objects.requireNonNull(System.getProperty("line.separator"))); }
         catch (Exception e) { e.printStackTrace(); }
         if(lines == null) return -1;
         temp = null;
@@ -117,6 +187,25 @@ public class CoronaVirusInfo {
                 return Integer.parseInt(Between(lines[3]));
         }
         return -1;
+    }
+
+    private String getType(@TYPE int type) {
+        switch (type) {
+            case 7:
+                return "Confirmed";
+
+            case 6:
+                return "Deaths";
+
+            case 5:
+                return "Recovered";
+
+            case 4:
+                return "Active";
+
+            default:
+                return null;
+        }
     }
 
     private String Between(String str) {
